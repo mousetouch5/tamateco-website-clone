837a8c64e17bb6ffaf924c8da19203fbf66ed951
joao <jvergara21@csab.edu.ph>
2025-11-04 00:18:26 +0800

    fix

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
   };
 
   return (
-    <header className="sfixed top-0 left-0 right-0 z-50 bg-black shadow-lg">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black shadow-lg">
       <div className="mx-auto w-full max-w-[1200px] px-12">
         {/* TOP BAR */}
         <div className="flex items-center h-28">
